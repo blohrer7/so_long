71d0d30e03a8481e34d092e0f46956db5bfc5f46
blohrer7 <benjamin-lohrer@web.de>
2025-03-05 08:04:55 +0100

    add so_long.h

---
+++ so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 08:00:43 by blohrer           #+#    #+#             */
+/*   Updated: 2025/03/05 08:02:34 by blohrer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "ft_printf.h"
+# include "libft.h"
+# include <limits.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+#endif
