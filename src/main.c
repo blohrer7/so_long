8f1e0033ab9d3a9520b11bd30926261ff16091d4
blohrer <you@example.com>
2025-03-10 08:44:42 +0100

    changed main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: blohrer <blohrer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:04:56 by blohrer           #+#    #+#             */
-/*   Updated: 2025/03/09 09:36:20 by blohrer          ###   ########.fr       */
+/*   Updated: 2025/03/10 08:41:29 by blohrer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,48 @@ int	main(int argc, char **argv)
 {
 	t_data	game;
 
+	game.moves = 0;
 	if (argc != 2)
 		return (ft_printf("Usage: %s <map_file.ber>\n", argv[0]), 1);
-	validate_map_file(argv[1]);
-	game.moves = 0;
 	game.map = load_map(argv[1], &game.width, &game.height);
-	if (!game.map || map_pce(&game) == -1)
+	if (!game.map)
 		return (1);
 	validate_map_characters(&game);
+	if (map_pce(&game) == -1)
+		return (ft_printf("Error: Invalid map (must contain 1P, at least 1C, and 1E)\n"), 1);
 	check_map_walls(&game);
 	get_player_position(&game);
+
+	// ✅ NEU: Überprüft, ob alle Collectibles und das Exit erreichbar sind
+	check_valid_path(&game);
+
 	init_window(&game);
 	render_map(&game);
 	mlx_key_hook(game.mlx, &handle_input, &game);
-	ft_printf("Game started! Press WASD to move.\n");
+	ft_printf("✅ Game started! Press WASD to move.\n");
 	mlx_loop(game.mlx);
 	return (0);
 }
+
+
+// int	main(int argc, char **argv)
+// {
+// 	t_data	game;
+
+// 	if (argc != 2)
+// 		return (ft_printf("Usage: %s <map_file.ber>\n", argv[0]), 1);
+// 	validate_map_file(argv[1]);
+// 	game.moves = 0;
+// 	game.map = load_map(argv[1], &game.width, &game.height);
+// 	if (!game.map || map_pce(&game) == -1)
+// 		return (1);
+// 	validate_map_characters(&game);
+// 	check_map_walls(&game);
+// 	get_player_position(&game);
+// 	init_window(&game);
+// 	render_map(&game);
+// 	mlx_key_hook(game.mlx, &handle_input, &game);
+// 	ft_printf("Game started! Press WASD to move.\n");
+// 	mlx_loop(game.mlx);
+// 	return (0);
+// }
